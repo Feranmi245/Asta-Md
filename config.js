6cdda8b4491e94661e5a1cba435fc99a0531fded
The joker <141430632+Feranmi245@users.noreply.github.com>
2024-04-30 00:40:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,20 +5,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "astromedia0010@gmail.com";
+global.email = "mofeoluwatestimony@gmail.com";
 global.location = "Lahore,Pakistan.";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/Astropeda/Asta-Md";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaPGt3QEwEjpBXT4Rv0z";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/ZkBjWK2.jpg";
-global.devs = "2348039607375";
-global.sudo = process.env.SUDO || "2348039607375";
-global.owner = process.env.OWNER_NUMBER || "2348039607375";
+global.github = process.env.GITHUB || "https://github.com/feranmi245/Asta-Md";
+global.gurl = process.env.GURL || "https://chat.whatsapp.com/C8f29luaMzT9lNUQ0UVyVm";
+global.website = process.env.GURL || "https://chat.whatsapp.com/C8f29luaMzT9lNUQ0UVyVm";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/2f17d744f49c0cab46e65.jpg";
+global.devs = "2348022159244";
+global.sudo = process.env.SUDO || "2348022159244";
+global.owner = process.env.OWNER_NUMBER || "2348022159244";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "ASTA").toUpperCase(),
+  LANG: (process.env.THEME || "NEZUKO").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
